98948ebc7a3f897e6e729bf63cff089a359b17fb
pavlopetr <rezar66612@gmail.com>
2022-08-01 18:13:37 +0300

    part 3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,49 @@
-import MainPage from "./MainPage/MainePage"
+import { Component } from 'react';
+import MainPage from './MainPage/MainePage';
+import TransactionHistoryPage from './TransactionHistoryPage/TransactionHistoryPage';
 
+export class App extends Component {
+  state = {
+    activePage: 'main',
+  };
 
+  onIncomesBtnClick = () => {
+    this.setState({ activePage: 'incomes' });
+  };
 
-export const App = () => {
-  return (
-    <div className="container">
-      <MainPage/>
-    
+  onCostsBtnClick = () => {
+    this.setState({ activePage: 'costs' });
+  };
+  onReturnBtnClick = () => {
+    this.setState({ activePage: 'main' });
+  };
 
-
-    </div>
-  );
-};
+  render() {
+    const { activePage } = this.state;
+    switch (activePage) {
+      case 'main':
+        return (
+          <MainPage
+            onIncomesBtnClick={this.onIncomesBtnClick}
+            onCostsBtnClick={this.onCostsBtnClick}
+          />
+        );
+      case 'costs':
+        return (
+          <TransactionHistoryPage
+            transType={activePage}
+            onReturnBtnClick={this.onReturnBtnClick}
+          />
+        );
+      case 'incomes':
+        return (
+          <TransactionHistoryPage
+            transType={activePage}
+            onReturnBtnClick={this.onReturnBtnClick}
+          />
+        );
+      default:
+        return;
+    }
+  }
+}
---
+++ src/components/TransactionHistoryPage/TransactionHistoryPage.jsx
@@ -0,0 +1,17 @@
+import Header from '../Header/Header';
+
+const TransactionHistoryPage = ({ transType, onReturnBtnClick }) => {
+    const transactionName = transType === 'costs' ? 'Costs' : 'Incomes';
+    return (
+      <div className="container">
+        <Header
+          title={`Transaction  ${transactionName}`}
+          icon={'#icon-arrow-left'}
+          cbOnClick={onReturnBtnClick}
+        />
+      </div>
+    );
+  };
+  
+
+export default TransactionHistoryPage;
